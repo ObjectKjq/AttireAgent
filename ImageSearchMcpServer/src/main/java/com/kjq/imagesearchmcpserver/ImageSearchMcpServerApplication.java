d18b9270754ee1e2eab7e964a0bd690e815420e5
kjq <1208230746@qq.com>
2025-06-02 17:07:46 +0800

    添加MCP服务

---
+++ ImageSearchMcpServer/src/main/java/com/kjq/imagesearchmcpserver/ImageSearchMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.kjq.imagesearchmcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSearchMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageSearchMcpServerApplication.class, args);
+    }
+
+}
---
+++ ImageSearchMcpServer/src/main/java/com/kjq/imagesearchmcpserver/tools/ImageSearchTool.java
@@ -0,0 +1,4 @@
+package com.kjq.imagesearchmcpserver.tools;
+
+public class ImageSearchTool {
+}
